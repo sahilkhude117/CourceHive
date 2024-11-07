87994e4c8cbcb261fb0c8181bd1782e3ba743dcb
Nikandr Surkov <nikandr.dev@gmail.com>
2024-11-08 00:34:42 +0700

    Initial project setup with basic structure and assets

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,8 @@
  * GitHub: https://github.com/nikandr-surkov
  */
 
-import CheckFootprint from '@/components/CheckFootprint'
-import NavigationBar from '@/components/NavigationBar'
-import TabContainer from '@/components/TabContainer'
-import { TabProvider } from '@/contexts/TabContext'
-
 export default function Home() {
   return (
-    <TabProvider>
-      <main className="min-h-screen bg-black text-white">
-        <div className="max-w-md mx-auto pt-[44px] pb-[72px]">
-          <CheckFootprint />
-
-          <TabContainer />
-        </div>
-        <NavigationBar />
-      </main>
-    </TabProvider>
+    <></>
   )
 }
