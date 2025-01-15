2cb8707e8a8a75a08706c6fd85dbf4102ba541a1
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-15 15:28:00 +0530

    added 3 tabs

--- components/TabContainer.tsx
+++ components/TabContainer.tsx
@@ -2,30 +2,21 @@
 
 import { useTab } from "@/contexts/TabContext";
 import HomeTab from "./HomeTab";
-import LeaderboardTab from "./LeaderboardTab";
-import FriendsTab from "./FriendsTab";
-import TasksTab from "./TasksTab";
 import { CourcesTab } from "./CourcesTab";
-import CourceDetailsPage from "./CourceDetailsPage";
-import CheckFootprint from "./CheckFootprint";
-import { useRouter } from "next/navigation";
+import Profile from "./Profile";
 
 const TabContainer = () => {
-    const router = useRouter();
-    const { activeTab } = useTab()
+    const { activeTab } = useTab();
     return (
         <div className="flex-1 overflow-hidden max-w-md mx-auto pt-[20px] pb-[72px]">
             <div className={`${activeTab === 'home' ? 'is-show' : 'is-hide'}`}>
                 <HomeTab/>
             </div>
-            <div className={`${activeTab === 'leaderboard' ? 'is-show' : 'is-hide'}`}>
-                <LeaderboardTab/>
-            </div>
-            <div className={`${activeTab === 'friends' ? 'is-show' : 'is-hide'}`}>
-                
-            </div>
-            <div className={`${activeTab === 'earn' ? 'is-show' : 'is-hide'}`}>
+            <div className={`${activeTab === 'cources' ? 'is-show' : 'is-hide'}`}>
                 <CourcesTab/>
+            </div>
+            <div className={`${activeTab === 'profile' ? 'is-show' : 'is-hide'}`}>
+                <Profile/>
             </div>
         </div>
     )
