598aec19e4559f75cc32d5509535622cd4dc6c39
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-15 15:26:41 +0530

    added navigation

--- components/CheckFootprint.tsx
+++ components/CheckFootprint.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import ArrowBigRight from "@/icons/ArrowBigRight";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useTab } from "@/contexts/TabContext";
 
 const CheckFootprint = () => {
+    const {activeTab,setActiveTab} = useTab();
     return (
         <div className="flex fixed z-20 justify-center w-full">
             <div className="top-0 w-full max-w-md px-4 py-3 bg-[#151516]">
                 <div className="flex justify-between items-center pl-2 border-1-[2px] border-[#4c9ce2]">
-                    <div className="flex text-base font-medium">
+                    <div className="flex text-base font-medium cursor-pointer" onClick={() => setActiveTab('home')} >
                         <div className="text-white">
                             Cource
                         </div>
@@ -16,8 +17,7 @@ const CheckFootprint = () => {
                             Hive
                         </div>
                     </div>
-                    
-                    <Avatar>
+                    <Avatar onClick={() => setActiveTab('profile')} className={`cursor-pointer`}>
                         <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
