aebb0176684dac97d25c0fe6b0fbe38fba48b537
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-15 14:01:28 +0530

    modified checkfootprint

--- components/CheckFootprint.tsx
+++ components/CheckFootprint.tsx
@@ -6,7 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 const CheckFootprint = () => {
     return (
         <div className="flex fixed z-20 justify-center w-full">
-            <div className="top-0 w-full max-w-md px-4 py-3 bg-[#151516] cursor-pointer">
+            <div className="top-0 w-full max-w-md px-4 py-3 bg-[#151516]">
                 <div className="flex justify-between items-center pl-2 border-1-[2px] border-[#4c9ce2]">
                     <div className="flex text-base font-medium">
                         <div className="text-white">
