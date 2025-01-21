5b7b9fb4a24f8234af4312c7b82a4e3348319e4f
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-21 12:36:37 +0530

    added be

--- components/CourcesTab.tsx
+++ components/CourcesTab.tsx
@@ -1,52 +1,55 @@
-import { taskWhitePaws } from "@/images"
+import { useEffect, useState } from "react";
 import CourseCard from "./CourceCard"
+import axios from "axios";
+import { CoursesSkeleton } from "./SkeletonCard";
+
+interface Cource {
+    id: string;
+    title: string;
+    slug: string;
+    description: string;
+    instructor: string;
+    thumbnailUrl: string;
+    duration: string;
+    price: number;
+    category: {
+        name: string;
+    }
+}
 
 export const CourcesTab = () => {
+    const [cources, setCources] = useState<Cource []>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchCources = async () => {
+            try {
+                const response = await axios.get('/api/courses');
+                setCources(response.data.cources);
+            } catch (error) {
+                console.error('Error fetching cources:', error);
+                setError("failed to fetch cources"); 
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCources();
+    }, []);
+
     return <div className="pt-20">
-        <CourseCard 
-            title = {"Cohort 2.0"} 
-            instructor = {"Harkirat Singh"}
-            thumbnail = {""}
-            duration = {"1w"}
-            progress = {50}
-            price = {"5000"}
-            category = {"random"}
-        />
-        <CourseCard 
-            title = {"Cohort 2.0"} 
-            instructor = {"Harkirat Singh"}
-            thumbnail = {""}
-            duration = {"1w"}
-            progress = {50}
-            price = {"5000"}
-            category = {"random"}
-        />
-        <CourseCard 
-            title = {"Cohort 2.0"} 
-            instructor = {"Harkirat Singh"}
-            thumbnail = {""}
-            duration = {"1w"}
-            progress = {50}
-            price = {"5000"}
-            category = {"random"}
-        />
-        <CourseCard 
-            title = {"Cohort 2.0"} 
-            instructor = {"Harkirat Singh"}
-            thumbnail = {""}
-            duration = {"1w"}
-            progress = {50}
-            price = {"5000"}
-            category = {"random"}
-        />
-        <CourseCard 
-            title = {"Cohort 2.0"} 
-            instructor = {"Harkirat Singh"}
-            thumbnail = {""}
-            duration = {"1w"}
-            progress = {50}
-            price = {"5000"}
-            category = {"random"}
-        />
+        {loading ? <CoursesSkeleton /> : cources.map((cource) => (
+            <CourseCard 
+                key = {cource.id}
+                title = {cource.title} 
+                instructor = {cource.instructor}
+                thumbnail = {cource.thumbnailUrl}
+                duration = {cource.duration}
+                price = {cource.price.toString()}
+                category = {cource.category.name}
+                slug = {cource.slug}
+            />
+        ))}
     </div>
 }
