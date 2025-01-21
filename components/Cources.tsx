d71c5af5b1e6b1a8513c218924bed5e7922a83f3
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-21 12:36:03 +0530

    updated courses component

--- components/Cources.tsx
+++ components/Cources.tsx
@@ -2,41 +2,40 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { courcehiveLogo, sparkles, taskWhitePaws } from '@/images';
 
 const NumberBackground = ({ number }: { number: number }) => {
   const num = number.toString().padStart(2, '');
   return (
-    <div className="absolute inset-0 flex justify-center items-center z-10">
-      <span className="text-[100px] text-white pt-20 opacity-20 font-bold transform -translate-y-10 -translate-x-10">{num}</span>
+    <div className="absolute inset-0 z-0">
+      <span className="text-[100px] text-white pt-20 pl-2 opacity-50 font-bold transform -translate-y-10 -translate-x-10">{num}</span>
     </div>
   );
 };
 
 const CourceItem = ({ item, router, index, isTop10 }: { 
-  item: { id: string; imageUrl: string }; 
+  item: { id: string; thumbnailUrl: string ; slug: string}; 
   router: any; 
   index: number; 
   isTop10: boolean; 
 }) => (
   <div
-    className={`relative cursor-pointer rounded-lg overflow-hidden h-[150px] flex-shrink-0 ${isTop10 ? "w-[150px]" : "w-[100px]"}`}
-    onClick={() => router.push(`/cources/${item.id}`)}
+    className={`relative cursor-pointer rounded-lg h-[120px] overflow-hidden flex-shrink-0 ${isTop10 ? "w-[250px] pl-10" : "w-[200px]"}`}
+    onClick={() => router.push(`/course/${item.slug}`)}
   >
     {isTop10 && <NumberBackground number={index + 1} />}
     <Image
-      src={taskWhitePaws}
+      src={item.thumbnailUrl}
       alt="Thumbnail"
-      className = {`w-full ${isTop10 ? "ml-10 h-full object-cover" : "h-full object-cover"} `}
+      className = {`w-full ${isTop10 ? "ml-1 h-full object-cover" : "h-full object-cover"} `}
       width={200}
-      height={225}
+      height={200}
     />
   </div>
 );
 
-const CourceList = ({ rowTitle, cources, type }: { 
-  rowTitle: string; 
-  cources: { id: string; imageUrl: string }[]; 
+const CourceList = ({ categoryName, cources, type }: { 
+  categoryName: string; 
+  cources: { id: string; thumbnailUrl: string , slug: string}[]; 
   type?: string; 
 }) => {
   const router = useRouter();
@@ -44,7 +43,7 @@ const CourceList = ({ rowTitle, cources, type }: {
 
   return (
     <div className="p-5">
-      <h2 className="text-xl font-bold mb-4">{rowTitle}</h2>
+      <h2 className="text-xl font-bold mb-4">{categoryName}</h2>
       <div className="flex gap-4 overflow-x-auto scrollbar-hide ">
         {cources.map((item, index) => (
           <CourceItem
