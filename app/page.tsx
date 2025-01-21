be18d4ca570deeb4988ad2a4c2d0c71676142289
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-21 17:28:23 +0530

    server session

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import CheckFootprint from "@/components/CheckFootprint";
 import NavigationBar from "@/components/NavigationBar";
 import TabContainer from "@/components/TabContainer";
 import { TabProvider } from "@/contexts/TabContext";
+import { NEXT_AUTH_CONFIG } from "@/public/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+async function getUser() {
+  const session = await getServerSession(NEXT_AUTH_CONFIG);
+  return session;
+}
+
+export default async function Home() {
+  const session = await getUser();
   return (
     <TabProvider>
       <main className="min-h-screen bg-black text-white">
