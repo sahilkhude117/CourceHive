1f0156656033fa3d3a0825af8d6a6c241785f3d8
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-21 12:33:33 +0530

    modified couses route

--- app/api/courses/route.tsx
+++ app/api/courses/route.tsx
@@ -3,7 +3,23 @@ import { NextResponse } from 'next/server';
 
 export const GET = async () => {
   try {
-    const cources = await prisma.course.findMany();
+    const cources = await prisma.course.findMany({
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        description: true,
+        instructor: true,
+        thumbnailUrl: true,
+        duration: true,
+        price: true,
+        category: {
+          select: {
+            name: true,
+          }
+        }
+      }
+    });
 
     return NextResponse.json(
       {
