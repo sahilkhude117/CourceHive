8af712a231fcb9142f075902919b81bfa7cb0745
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-23 11:36:15 +0530

    updated slug route

---
+++ app/api/course/[slug]/route.tsx
@@ -0,0 +1,50 @@
+import prisma from '@/utils/db';
+import { NextResponse, NextRequest } from 'next/server';
+
+export const GET = async (req: NextRequest, { params }: { params: { slug: string } }) => {
+  try {
+    const courseSlug = params.slug;
+
+    const course = await prisma.course.findUnique({
+      where: {
+        slug: courseSlug ?? undefined
+      },
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        description: true,
+        instructor: true,
+        thumbnailUrl: true,
+        duration: true,
+        price: true,
+        telegramLink: true,
+        category: {
+          select: {
+            name: true,
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(
+      {
+        course
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    console.log('Error', e);
+    return NextResponse.json(
+      {
+        error: 'Internal Server Error',
+        details: e,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
