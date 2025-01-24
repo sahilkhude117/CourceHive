a55b9171dab989125ed17ac668a42eab487cff6f
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-24 12:36:02 +0530

    added original Cource price

--- components/Cources.tsx
+++ components/Cources.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { Badge } from './ui/badge';
 
 const NumberBackground = ({ number }: { number: number }) => {
   const num = number.toString().padStart(2, '');
