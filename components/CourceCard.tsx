b41bf619a9043545969bf2246049d9712326c25f
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-24 12:35:33 +0530

    off badge

--- components/CourceCard.tsx
+++ components/CourceCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { PlayCircle, Clock} from 'lucide-react';
+import { PlayCircle, Clock, IndianRupee} from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { ShineBorder } from './ui/shine-border';
 
 const CourseCard = ({ 
   title, 
@@ -11,6 +12,7 @@ const CourseCard = ({
   thumbnail, 
   duration, 
   slug,
+  originalPrice,
   price, 
   category 
 }:{
@@ -19,10 +21,13 @@ const CourseCard = ({
     thumbnail : string, 
     duration : string,  
     slug : string,
-    price : string, 
+    originalPrice: number,
+    price : number, 
     category : string   
 }) => {
     const router = useRouter();
+    const discountPecentage = Math.round(((originalPrice - price) / originalPrice) * 100);
+
   return (
     <div className='ml-5 mb-5 mr-5'>
         <Card className="w-full max-w-sm mx-auto">
@@ -31,13 +36,20 @@ const CourseCard = ({
             <Image
                 src={thumbnail}
                 alt="courceHive Logo"
-                className='w-full h-48 object-cover rounded-t-lg'
+                className='w-full h-52 object-cover rounded-t-lg'
                 width={500}
                 height={500}
             />
             <Badge className="absolute top-2 right-2 bg-primary">
                 {category}
             </Badge>
+            <ShineBorder borderRadius={8} borderWidth={2} className="absolute top-2 left-2 text-xs">
+                <div className='rounded-full'>
+                    <div className='font-bold'>
+                        {discountPecentage}% Off
+                    </div>
+                </div>
+            </ShineBorder>
             </div>
         </CardHeader>
         <CardContent className="p-4">
@@ -57,7 +69,20 @@ const CourseCard = ({
             </div>
         </CardContent>
         <CardFooter className="p-4 pt-0 flex justify-between items-center">
-            <span className="text-lg font-bold">₹{price}</span>
+            <div>
+                <div>
+                    <div className='flex flex-row'>
+                        <IndianRupee size={12} className='mt-2'/>
+                        <span className="text-3xl font-bold">{price}</span>
+                    </div>
+                    
+                    <div>
+                    <span className="text-red-900 font-bold text-sm pr-1">- {discountPecentage}% </span>
+                        <span className="text-gray-500 line-through text-sm">₹{originalPrice}</span>
+                    </div>
+                </div>
+            </div>
+            
             <button onClick={() => {
                 router.push(`/course/${slug}`);
             }} className="bg-[#4c9ce2] text-black px-4 py-2 rounded-lg text-sm font-bold">
