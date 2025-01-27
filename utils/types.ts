9319cd34e0c8489c7b8460b6ee1b9d7090a3909e
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-27 13:20:30 +0530

    phonepe payload types

--- utils/types.ts
+++ utils/types.ts
@@ -5,4 +5,17 @@ export type IconProps = {
     className?: string;
 }
 
+export interface PhonePePayload {
+    merchantId: string;
+    merchantTransactionId: string;
+    merchantUserId: string;
+    amount: number;
+    redirectUrl: string;
+    redirectMode: string;
+    callbackUrl: string;
+    paymentInstrument: {
+        type: string;
+    }
+}
+
 export type TabType = 'home' | 'cources' | 'profile';
