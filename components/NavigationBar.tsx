8a2683a72b72917f4b6f1663597221aca1880152
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-30 15:20:15 +0530

    added my courses tab

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import Home from "@/icons/Home";
-import Friends from "@/icons/Friends";
 import { TabType } from "@/utils/types";
 import { useTab } from "@/contexts/TabContext";
-import Cources from "@/icons/Cources";
+import { BookOpen, GraduationCap, HomeIcon, Terminal, UserCircle } from "lucide-react";
+
 
 const NavigationBar = () => {
     const {activeTab,setActiveTab} = useTab();
     const tabs: {id:TabType; label:string; Icon: React.FC<{className?: string}>}[] = [
-        {id: 'home', label: 'home', Icon: Home},
-        {id: 'cources', label: 'cources', Icon: Cources},
-        {id: 'profile', label: 'profile', Icon: Friends},
+        {id: 'home', label: 'Home', Icon: HomeIcon},
+        {id: 'cources', label: 'Cources', Icon: Terminal},
+        {id: 'mycources', label: 'MyCources', Icon: BookOpen},
+        {id: 'profile', label: 'Profile', Icon: UserCircle},
     ]
 
     return (
@@ -27,7 +27,7 @@ const NavigationBar = () => {
                                 className={`flex flex-col items-center`}
                             >
                                 <tab.Icon
-                                    className={`w-10 h-10 ${isActive ? `text-[#4c9ce2]` : `text-[#727272]`}`}
+                                    className={`w-8 h-8 ${isActive ? `text-[#4c9ce2]` : `text-[#727272]`}`}
                                 />
                                 <span>
                                     {tab.label}
